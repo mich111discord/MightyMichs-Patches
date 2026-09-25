0bfb653df742e7311659507844501a8b9d865b0c
MightyMich <141028293+mich111discord@users.noreply.github.com>
2026-09-25 19:55:24 +0200

    fix: Mago video patch fix

--- patches/src/main/kotlin/mightymich/morphe/patches/mobi.charmer.magovideo/UnlockPremium.kt
+++ patches/src/main/kotlin/mightymich/morphe/patches/mobi.charmer.magovideo/UnlockPremium.kt
@@ -10,7 +10,7 @@ import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
 @Suppress("unused")
 val unlockPremiumPatch = bytecodePatch(
     name = "Unlock Premium Features",
-    description = "Unlocks premium features in MagoVideo by forcing the premium check to return true."
+    description = "Unlocks premium features in MagoVideo by forcing the premium check to return true. "
 ) {
     compatibleWith(MagoVideoCompatibility.MAGO_VIDEO)
 
