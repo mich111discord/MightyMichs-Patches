ee35378203109c74c5aa002712e8ae7f8364bfa4
MightyMich <141028293+mich111discord@users.noreply.github.com>
2026-09-26 19:46:25 +0200

    fix: ...

--- patches/src/main/kotlin/mightymich/morphe/patches/videoeditor.videomaker.videoeditorforyoutube/VideoGuruCompatibility.kt
+++ patches/src/main/kotlin/mightymich/morphe/patches/videoeditor.videomaker.videoeditorforyoutube/VideoGuruCompatibility.kt
@@ -6,13 +6,13 @@ import app.morphe.patcher.patch.Compatibility
 
 object VideoGuruCompatibility {
     val VIDEO_GURU = Compatibility(
-        name = "Video Maker", // App name shown in Morphe.
+        name = "Video Guru",
         packageName = "videoeditor.videomaker.videoeditorforyoutube",
         apkFileType = ApkFileType.APK,
         appIconColor = 0xFF9800, // Orange color, typical for video editing apps.
         targets = listOf(
             AppTarget(version = "1.371.93")
-            AppTarget(version = "1.621.196") 
+            AppTarget(version = "1.621.196") // Tested version.
         )
     )
 }
